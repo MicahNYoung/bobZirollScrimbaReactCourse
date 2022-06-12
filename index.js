905db42e1b964d14db20e5182d3d4948436a504a
Micah <micahnyoung95@gmail.com>
2022-06-12 13:37:13 -0500

    static website, didn't download image

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const page = (
         <img src="./react-logo.png"></img>
         <h1>Fun facts about React</h1>
         <ul>
-            <li>Was furst released in 2013</li>
+            <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
             <li>Has well over 100k stars on Github</li>
             <li>Is maintained by Facebook</li>
