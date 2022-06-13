1d37c50d1affcb13cacf9dbe4e5e7e73283a859a
Micah <micahnyoung95@gmail.com>
2022-06-13 12:28:34 -0500

    starting parentheses need to be on same line as return

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
+function Header(){
+    return (<header>
+                <nav>
+                    <img src="./reactLogo.png" width="50px"></img>
+                </nav>
+        </header>
+    )
+}
+function MainContent(){
+    return (
+        <div>
+            <h1>Reasons learning React is exciting</h1>
+                <ul>
+                    <li>It's tangentially related to languages/frameworks I've work with before</li>
+                    <li>It's an in demand framework</li>
+                    <li>It's a useful skill to build full stack applications</li>
+                </ul>
+        </div>
+        )
+}
+
+function Footer() {
+    return (
+        <footer>
+                    <small>© 2022 Young Development. All rights reserved.</small>
+        </footer>
+    )
+    
+}
 function ReasonsReactIsExciting(){
     return (  
         <div>
-            <header>
-                <nav>
-                    <img src="./reactLogo.png" width="50px"></img>
-
-                </nav>
-            </header>
-            <h1>Reasons learning React is exciting</h1>
-            <ul>
-                <li>It's tangentially related to languages/frameworks I've work with before</li>
-                <li>It's an in demand framework</li>
-                <li>It's a useful skill to build full stack applications</li>
-             </ul>
-             <footer>
-                 <small>© 2022 Young Development. All rights reserved.</small>
-            </footer>
+            <Header/>
+            <MainContent/>
+             <Footer />
         </div>)
-    
-      
-        
         
 }
 
