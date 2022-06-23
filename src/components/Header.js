23226df872c9232b335b265acbba3d30baeb60d2
Micah <micahnyoung95@gmail.com>
2022-06-23 13:22:00 -0500

    beginning of memeGenerator

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import trollFace from "images/memeGenerator/trollFace.png"
+
+export function Header() {
+    return (
+        <div>
+            <img src="images/memeGenerator/trollFace.png"></img>
+            <h1>Meme Generator</h1>
+            <h4>React Course - Project 3</h4>
+        </div>
+    )
+}
